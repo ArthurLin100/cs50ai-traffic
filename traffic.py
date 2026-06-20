51267e939250fd8814372dde5e4af3c40d284b81
Arthur Lin <thur.lin@gmail.com>
2026-06-20 17:02:38 +0900

    sytle50 checked

--- traffic.py
+++ traffic.py
@@ -91,30 +91,30 @@ def get_model():
     # Create a convolutional neural network
     model = tf.keras.models.Sequential([
 
-    # Convolutional layer. Learn 32 filters using a 3x3 kernel
-    tf.keras.layers.Conv2D(
-        32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
-    ),
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
 
-    # Max-pooling layer, using 2x2 pool size
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-    # Flatten units
-    tf.keras.layers.Flatten(),
+        # Flatten units
+        tf.keras.layers.Flatten(),
 
-    # Add a hidden layer with dropout
-    tf.keras.layers.Dense(128, activation="relu"),
-    tf.keras.layers.Dropout(0.5),
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
 
-    # === additional hidden layer with dropout ===
-    # tf.keras.layers.Dense(128, activation="relu"),    
-    # tf.keras.layers.Dense(128, activation="relu"),
-    # tf.keras.layers.Dense(128, activation="relu"),
-    # tf.keras.layers.Dense(128, activation="relu"),
-    # tf.keras.layers.Dropout(0.5),        
+        # === additional hidden layer with dropout ===
+        # tf.keras.layers.Dense(128, activation="relu"),    
+        # tf.keras.layers.Dense(128, activation="relu"),
+        # tf.keras.layers.Dense(128, activation="relu"),
+        # tf.keras.layers.Dense(128, activation="relu"),
+        # tf.keras.layers.Dropout(0.5),        
 
-    # Add an output layer with output units for all 10 digits
-    tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
 
     # Train neural network
